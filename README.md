c953db92cfdbec4831ee9ec1125e72bdb9726f63
RedForty <daniel@redforty.com>
2017-09-12 00:15:54 -0700

    First big commit
    BOOSH BOOSH BOOSH

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 mmViewSelect
+
+---
+**[Usage](#usage)**
+
+Put the mmViewSelect.py file in your <MY_DOCUMENTS>/maya/scripts/ folder.
+
+Now you need to make two hotkey commands.
+Open the maya hotkey editor and open the Runtime Command Editor.
+Hit 'New' and give it a name like:
+<b>mmViewSelect_PRESS</b>
+
+Paste this code into the command area:
+```
+import mmViewSelect
+mmViewSelect.press()
+```
+Make sure to switch the language to <b>python</b>.
+When ready, hit Save.
+
+Hit 'New' again and this time name it:
+<b>mmViewSelect_RELEASE</b>
+
+Paste this code into the command area:
+```
+mmViewSelect.release()
+```
+Again, make sure to switch the language to <b>python</b>.
+When ready, hit Save.
+
+---
+
+Now assign both to the same key. One press and the other release.
+
+
---
+++ mmViewSelect.py
@@ -0,0 +1,159 @@
+from maya import cmds, mel
+import maya.api.OpenMayaUI as mui
+import maya.api.OpenMaya as api
+import time
+
+#------------------------------------------------------------------------------#
+# GLOBALS
+#------------------------------------------------------------------------------#
+
+global TIME_START
+global SHOT_CAM
+
+MENU_NAME        = "mmViewSelect"
+TIME_START       = 0.0
+SHOT_CAM         = "persp"
+CURRENT_PANEL    = ""
+MM_REGISTRY      = {}
+
+# ---------------------------------------------------------------------------- #
+
+class ViewportMarkingMenu(object):
+    def __init__(self, name):
+        # Data type to hold panel and associated callbackID
+        self.name = name
+        self.callbackRegistry = {}
+        self.toggleCamQueue = self._setupCamQueue()
+
+        self._installCallback()
+
+    def run(self):
+        self._removeOld()
+        self._build()
+
+    def camToggle(self):
+        if isPanel():
+            cmds.lookThru(self.toggleCamQueue[1])
+
+    def _setupCamQueue(self):
+        # Start with a working camera queue
+        currentCamera = self._getCurrentCamera()
+        return [currentCamera, currentCamera]
+
+    def _cameraChangeCallback(self, panel, curCamMObj, clientDataObj):
+        fnCam = api.MFnCamera(curCamMObj) # perspShape
+        parentObj = api.MFnTransform(fnCam.parent(0)) # persp
+        self.toggleCamQueue.pop(-1)
+        self.toggleCamQueue.insert(0, parentObj.name())
+
+    def _installCallback(self):
+        currentPanel = isPanel()
+        if currentPanel:
+            if not currentPanel in self.callbackRegistry:
+                print "Added callback for %s" % currentPanel
+                self.callbackRegistry[currentPanel] = mui.MUiMessage.addCameraChangedCallback(currentPanel, self._cameraChangeCallback)
+
+    def _removeCallback(self):
+        # Just in case I need to reset
+        for currentPanel in self.callbackRegistry:
+            if self.callbackRegistry[currentPanel]:
+                self.callbackRegistry[currentPanel] = mui.MUiMessage.removeCallback(self.callbackRegistry[currentPanel])
+
+    def _removeOld(self):
+        if cmds.popupMenu(self.name+MENU_NAME, ex=1):
+            cmds.deleteUI(self.name+MENU_NAME)
+
+    def _build(self):
+        menu = cmds.popupMenu(self.name+MENU_NAME,
+                              markingMenu         = 1,
+                              allowOptionBoxes    = 1,
+                              button              = 1,
+                              ctrlModifier        = 0,
+                              altModifier         = 0,
+                              shiftModifier       = 0,
+                              parent              = "viewPanes",
+                              postMenuCommandOnce = 1,
+                              postMenuCommand     = self._buildMarkingMenu)
+
+    def _getCurrentCamera(self):
+        if isPanel():
+            return cmds.lookThru(q=True)
+
+    def _buildMarkingMenu(self, menu, parent):
+        ## Radial positioned
+        cmds.menuItem(p=menu, l="persp", rp="N", c=perspView)
+        cmds.menuItem(p=menu, l="side", rp="E", c=sideView)
+        cmds.menuItem(p=menu, l="front", rp="S", c=frontView)#, i="mayaIcon.png")
+        cmds.menuItem(p=menu, l="top", rp="W", c=topView)
+        cmds.menuItem(p=menu, l="shotCam", rp="NW", c=camView)
+        cmds.menuItem(p=menu, ob=1, c=setShotCam)
+
+        ## List
+        #cmds.menuItem(p=menu, l="ackToggleCams", c=ackToggleCams)
+        #cmds.menuItem(p=menu, l="Rebuild Marking Menu", c=rebuildMarkingMenu)
+
+# ---------------------------------------------------------------------------- #
+
+def press(*args):
+    global CURRENT_PANEL
+    CURRENT_PANEL = isPanel()
+    if isPanel():
+        # Add to the registry
+        if not (CURRENT_PANEL in MM_REGISTRY):
+            MM_REGISTRY[CURRENT_PANEL] = ViewportMarkingMenu(CURRENT_PANEL)
+        # Deploy
+        MM_REGISTRY[CURRENT_PANEL].run()
+
+        # Start the timer
+        global TIME_START
+        TIME_START = time.time()
+
+def release(*args):
+    global CURRENT_PANEL
+    global TIME_START
+    if CURRENT_PANEL:
+        if cmds.popupMenu(CURRENT_PANEL+MENU_NAME, ex=1):
+            cmds.deleteUI(CURRENT_PANEL+MENU_NAME)
+        if (time.time() - TIME_START) < 0.15:
+            MM_REGISTRY[CURRENT_PANEL].camToggle()
+
+def isPanel(*args):
+    currentPanel = cmds.getPanel(wf=True)
+    if cmds.getPanel(to=currentPanel) == 'modelPanel':
+        return currentPanel
+    else:
+        return False
+
+def perspView(*args):
+    if isPanel():
+        cmds.lookThru('persp')
+
+def camView(*args):
+    global SHOT_CAM
+    if isPanel():
+        cmds.lookThru(SHOT_CAM)
+
+def frontView(*args):
+    if isPanel():
+        cmds.lookThru('front')
+
+def sideView(*args):
+    if isPanel():
+        cmds.lookThru('side')
+
+def topView(*args):
+    if isPanel():
+        cmds.lookThru('top')
+
+def setShotCam(*args):
+    global SHOT_CAM
+    if isPanel():
+        SHOT_CAM = cmds.lookThru(q=True)
+
+def _powerWordKILL(*args):
+    # Just in case I need to reset
+    for instance in MM_REGISTRY:
+        MM_REGISTRY[instance]._removeCallback()
+
+
+
